ea0c731226fda7f7e6d9d886e5e574679f85215e
vish182 <vishwajitds@gmail.com>
2021-10-16 19:52:09 +0530

    filter and refresh

--- src/api/index.js
+++ src/api/index.js
@@ -12,6 +12,30 @@ export const getRecords = ({ offset }) => {
       return response.json();
     })
     .catch((err) => {
+      console.log("error: ", err);
+    });
+};
+
+export const getFilteredRecords = ({ offset, conditions }) => {
+  //console.log(user.name, user.email, user.password, user.phone);
+  // console.log(`${API}/filteredrecords/${offset}`);
+  // console.log(conditions);
+  console.log("srtingify: ", offset, JSON.stringify(conditions));
+  console.log("srtingify: ", conditions);
+  return fetch(`${API}/filteredrecords/${offset}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ conditions: conditions }),
+  })
+    .then((response) => {
+      //console.log(JSON.stringify(response.json()));
+      console.log(response);
+      return response.json();
+    })
+    .catch((err) => {
       console.log(err);
     });
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import { Margin } from "@material-ui/icons";
 import React, { useEffect, useState } from "react";
 
-import { getRecords } from "../api";
+import { getRecords, getFilteredRecords } from "../api";
 
 import "../styles/home.css";
 
@@ -10,22 +10,58 @@ export const Records = () => {
 
   const [offset, setOffset] = useState(0);
 
-  const loadRecords = ({ pOffset }) => {
-    getRecords({ offset: pOffset }).then((data) => {
-      console.log("data: ", data);
-      if (!data || data.error) {
-        console.log(data.error);
-      } else {
-        console.log("on home : ", data);
-        setRows(data);
+  const [filters, setFilters] = useState({
+    jobCode: "",
+    customer: "",
+    company: "",
+    queryConditions: "",
+  });
+
+  const { jobCode, customer, company, queryConditions } = filters;
+
+  // const loadRecords = ({ pOffset }) => {
+  //   console.log(`called load records at ${Date.now()}`);
+  //   getRecords({ offset: pOffset }).then((data) => {
+  //     console.log("data: ", data);
+  //     if (!data || data.error) {
+  //       console.log(data.error);
+  //     } else {
+  //       console.log("on home : ", data);
+  //       setRows(data);
+  //     }
+  //   });
+  // };
+
+  const loadRecords = ({ pOffset, conditions }) => {
+    console.log("args: ", pOffset, conditions);
+    console.log(`called load records at ${Date.now()}`);
+    getFilteredRecords({ offset: pOffset, conditions: conditions }).then(
+      (data) => {
+        console.log("data: ", data);
+        if (!data || data.error) {
+          console.log(data);
+        } else {
+          console.log("on home : ", data);
+          setRows(data);
+        }
       }
-    });
+    );
   };
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    loadRecords({ pOffset: offset });
+    loadRecords({ pOffset: offset, conditions: queryConditions });
   }, [offset]);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => loadRecords({ pOffset: offset, conditions: queryConditions }),
+      30000
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, [queryConditions]);
 
   const nextRecords = () => {
     setOffset(offset + 25);
@@ -36,14 +72,91 @@ export const Records = () => {
     setOffset(offset - 25);
   };
 
+  const handleChange = (fieldName) => {
+    // higher order function
+    return (event) => {
+      let value;
+      value = event.target.value;
+
+      setFilters({ ...filters, [fieldName]: value });
+    };
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let conditions = "";
+    let flag = false;
+
+    if (jobCode != "") {
+      conditions += ` AND JobCode = '${jobCode}'`;
+    }
+
+    if (customer != "") {
+      conditions += ` AND Customer = '${customer}'`;
+    }
+
+    if (company != "") {
+      conditions += ` AND Company = '${company}'`;
+    }
+
+    setFilters({ ...filters, queryConditions: conditions });
+
+    console.log("conditions: ", conditions);
+
+    loadRecords({ pOffset: offset, conditions: queryConditions });
+  };
+
+  const filterForm = () => {
+    // onSubmit={clickSubmit}
+    return (
+      <form className="mb-3" onSubmit={handleSubmit}>
+        {JSON.stringify(filters)}
+        <div className="form-row">
+          <div className="form-group ml-2">
+            <label className="text-muted">Job Code</label>
+            <input
+              type="text"
+              onChange={handleChange("jobCode")}
+              className="form-control"
+              // value={jobCode}
+            />
+          </div>
+
+          <div className="form-group ml-2">
+            <label className="text-muted">Customer</label>
+            <input
+              type="text"
+              onChange={handleChange("customer")}
+              className="form-control"
+              // value={customer}
+            />
+          </div>
+
+          <div className="form-group ml-2">
+            <label className="text-muted">Company</label>
+            <input
+              type="text"
+              onChange={handleChange("company")}
+              className="form-control"
+              // value={company}
+            />
+          </div>
+        </div>
+        <button className="btn btn-outline-primary ml-2">Submit</button>
+      </form>
+    );
+  };
+
   return (
     <div>
+      {filterForm()}
       <div className="next-prev" style={{ margin: "10px" }}>
         <button className="prev-btn" onClick={prevRecords}>
           Previous
         </button>
         <div className="records-heading">
-          <h2>Fetched Records ..</h2>
+          <h2>Records</h2>
         </div>
         <button className="next-btn" onClick={nextRecords}>
           Next
@@ -66,7 +179,7 @@ export const Records = () => {
           </tr>
           {rows.map((row, i) => {
             return (
-              <tr>
+              <tr key={i}>
                 <td>{row.TaskName}</td>
                 <td>{row.JobCode}</td>
                 <td>{row.User}</td>
