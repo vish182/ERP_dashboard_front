8be075ca4e5153f29621af66ef286e01e830599b
vish182 <vishwajitds@gmail.com>
2021-10-18 00:20:18 +0530

    colour codes and update

--- src/api/index.js
+++ src/api/index.js
@@ -40,6 +40,33 @@ export const getFilteredRecords = ({ offset, conditions }) => {
     });
 };
 
+export const updateJobStatus = ({ jobKey, updateMessage, jobStatus }) => {
+  //console.log(user.name, user.email, user.password, user.phone);
+  // console.log(`${API}/filteredrecords/${offset}`);
+  // console.log(conditions);
+  console.log("srtingify: ", jobKey, updateMessage, jobStatus);
+  return fetch(`${API}/update_job_status`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jobKey: jobKey,
+      updateMessage: updateMessage,
+      jobStatus: jobStatus,
+    }),
+  })
+    .then((response) => {
+      //console.log(JSON.stringify(response.json()));
+      console.log(response);
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 export const getExecutionTypes = () => {
   //console.log(user.name, user.email, user.password, user.phone);
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,6 @@
-import { Margin } from "@material-ui/icons";
 import React, { useEffect, useState } from "react";
-
-import { getRecords, getFilteredRecords } from "../api";
+import { Modal, Box, Typography, Button } from "@material-ui/core";
+import { updateJobStatus, getFilteredRecords } from "../api";
 
 import "../styles/home.css";
 
@@ -15,9 +14,19 @@ export const Records = () => {
     customer: "",
     company: "",
     queryConditions: "",
+    updateMessage: "",
   });
 
-  const { jobCode, customer, company, queryConditions } = filters;
+  const [updateOpen, setUpdateOpen] = useState(false);
+  const [messageOpen, setMessageOpen] = useState(false);
+
+  const [jobKeyState, setJobKeyState] = useState(-1);
+  const [jobStatus, setJobStatus] = useState("Pending");
+
+  const [message, setMessage] = useState("");
+
+  const { jobCode, customer, company, queryConditions, updateMessage } =
+    filters;
 
   // const loadRecords = ({ pOffset }) => {
   //   console.log(`called load records at ${Date.now()}`);
@@ -82,7 +91,7 @@ export const Records = () => {
     };
   };
 
-  const handleSubmit = (e) => {
+  const handleFilterSubmit = (e) => {
     e.preventDefault();
 
     let conditions = "";
@@ -110,7 +119,7 @@ export const Records = () => {
   const filterForm = () => {
     // onSubmit={clickSubmit}
     return (
-      <form className="mb-3" onSubmit={handleSubmit}>
+      <form className="mb-3" onSubmit={handleFilterSubmit}>
         {JSON.stringify(filters)}
         <div className="form-row">
           <div className="form-group ml-2">
@@ -148,6 +157,179 @@ export const Records = () => {
     );
   };
 
+  const statusLabel = ({ status }) => {
+    let color = "white";
+
+    switch (status) {
+      case "Pending":
+        color = "red";
+        break;
+
+      case "WIP":
+        color = "yellow";
+        break;
+
+      case "Solved":
+        color = "green";
+        break;
+
+      default:
+        break;
+    }
+
+    return <td style={{ backgroundColor: `${color}` }}>{status}</td>;
+  };
+
+  // const updateHandler = ({ jobKey }) => {
+  //   return () => {
+  //     console.log(jobKey);
+  //   };
+  // };
+
+  // const statusButton = ({ handler }) => {
+  //   return (
+  //     <td style={{ display: "flex", justifyContent: "center" }}>
+  //       <button className="btn btn-outline-primary " onClick={handler}>
+  //         Update
+  //       </button>
+  //     </td>
+  //   );
+  // };
+
+  const messageModal = ({ text }) => {
+    const style = {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 400,
+      bgcolor: "background.paper",
+      border: "2px solid #000",
+      // boxShadow: 24,
+      p: 4,
+    };
+
+    const handleOpen = () => {
+      setMessage(text);
+      setMessageOpen(true);
+    };
+    const handleClose = () => setMessageOpen(false);
+
+    return (
+      <td>
+        <Button onClick={handleOpen}>View</Button>
+        <Modal
+          open={messageOpen}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Engineer Message
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {message}
+            </Typography>
+          </Box>
+        </Modal>
+      </td>
+    );
+  };
+
+  const handeStatusDrop = (e) => {
+    setJobStatus(e.target.value);
+  };
+
+  const updateModal = ({ jobKey, pJobStatus }) => {
+    const style = {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 400,
+      bgcolor: "background.paper",
+      border: "2px solid #000",
+      // boxShadow: 24,
+      p: 4,
+    };
+
+    const handleOpen = () => {
+      setUpdateOpen(true);
+      console.log(jobKey);
+      setJobKeyState(jobKey);
+      setJobStatus(pJobStatus);
+    };
+    const handleClose = () => {
+      setUpdateOpen(false);
+      setFilters({ ...filters, updateMessage: "" });
+      setJobKeyState(-1);
+    };
+
+    const handleUpdateSubmit = (e) => {
+      e.preventDefault();
+      setUpdateOpen(false);
+      console.log(jobKeyState);
+      console.log(jobStatus);
+
+      updateJobStatus({
+        jobKey: jobKeyState,
+        jobStatus: jobStatus,
+        updateMessage: updateMessage,
+      }).then((data) => {
+        if (!data || data.error) {
+          console.log(data);
+        } else {
+          console.log("success");
+          alert("Update successful");
+        }
+      });
+    };
+
+    return (
+      <td>
+        <Button onClick={handleOpen}>Update</Button>
+        <Modal
+          open={updateOpen}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <form className="mb-3" onSubmit={handleUpdateSubmit}>
+              {JSON.stringify(filters)}
+              {JSON.stringify(jobStatus)}
+              {JSON.stringify(jobKeyState)}
+              <div className="form-row">
+                <div className="form-group ml-2">
+                  <label className="text-muted">Update Engineer Message</label>
+                  <textarea
+                    type="text"
+                    onChange={handleChange("updateMessage")}
+                    className="form-control"
+                    // value={jobCode}
+                  />
+                  <label className="text-muted mt-5">Update Status</label>
+                  <div>
+                    <select className="btn mr-2" onChange={handeStatusDrop}>
+                      <option value="Pending">Pending</option>
+                      <option value="WIP">WIP</option>
+                      <option value="Solved">Solved</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <button className="btn btn-outline-primary mt-2">Submit</button>
+            </form>
+          </Box>
+        </Modal>
+      </td>
+    );
+  };
+
   return (
     <div>
       {filterForm()}
@@ -176,6 +358,9 @@ export const Records = () => {
             <th>TerminatedOn</th>
             <th>ExecutionType</th>
             <th>MailSent</th>
+            <th>Engg Status</th>
+            <th>Engg Message</th>
+            <th>Update</th>
           </tr>
           {rows.map((row, i) => {
             return (
@@ -190,6 +375,13 @@ export const Records = () => {
                 <td>{row.TerminatedOn}</td>
                 <td>{row.ExecutionType}</td>
                 <td>{row.MailSent}</td>
+                {statusLabel({ status: row.EnggStatus })}
+                {messageModal({ text: row.EnggMessage })}
+                {/* <td>{row.EnggMessage}</td> */}
+                {updateModal({
+                  jobKey: row.JobKey,
+                  pJobStatus: row.EnggStatus,
+                })}
               </tr>
             );
           })}
