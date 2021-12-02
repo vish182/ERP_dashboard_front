f8749cc26f10d7ab9b54913a2c732df3cc864ebb
Vishwajit Shelke <59522615+vish182@users.noreply.github.com>
2021-12-02 11:03:06 +0530

    Update firebase_config.js

--- src/auth/firebase_config.js
+++ src/auth/firebase_config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCO03eiMYR1lIc86AyL69PYW7WLGDl9ziU",
-  authDomain: "emphasys-auth.firebaseapp.com",
-  projectId: "emphasys-auth",
-  storageBucket: "emphasys-auth.appspot.com",
-  messagingSenderId: "512700257785",
-  appId: "1:512700257785:web:2b21b37a3167c87c021928",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
